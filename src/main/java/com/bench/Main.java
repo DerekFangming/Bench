e1b89373b5307482aa6d859e66ea000bf7c7f7c4
Fangming Ning <synfm@126.com>
2020-04-23 18:04:13 -0500

    Update Main.java

--- src/main/java/com/bench/Main.java
+++ src/main/java/com/bench/Main.java
@@ -3,6 +3,6 @@ package com.bench;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world from benchmark"); 
+        System.out.println("Hello world from benchmark");  
     }
 }
